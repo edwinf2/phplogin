67bc15343f32abe31bd066bf127a0014c71e2957
Edwin Figueroa <edwinf2020@gmail.com>
2019-09-13 02:51:56 +0200

    subida de archivo

---
+++ conexion.php
@@ -0,0 +1,14 @@
+<?php
+include ("configuracion.php");
+$conexion = new mysqli($server,$user,$pass,$bd);
+if (mysqli_connect_errno()) {
+    echo "No conectado", mysqli_connect_errno();
+    exit();
+} /*else {
+    echo 'Conectado';
+}*/
+
+
+
+
+?>
